6dccd568c24e92920f6b9d46803a0dc9c1464ee9
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-06-06 17:59:26 -0300

    trata o campo de valor na mudanca da opcao campo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const fixedValuesTypes = ['select','radio'];
 const todosOperadores = [ '=' , 'possui' , '<' , '>' , '>=' , '<=' ];
 function preencheValores( idCampo , selectValor , valor ) {
+    selectValor.empty().append($('<option>CARREGANDO</option>'));
     $.ajax('./ajax/getValues.php', {
         data: { id: idCampo },
         method: 'get',
         success: function ( response ) {
+            selectValor.empty();
             const opcoes = JSON.parse(response);
             for (let i = 0; i < opcoes.length; i++) {
                 const $opt = $('<option value="'+opcoes[i]+'">'+opcoes[i]+'</option>');
@@ -18,10 +20,12 @@ function preencheValores( idCampo , selectValor , valor ) {
 }
 
 function preencheCampo( categoria , selectCampo, selectValor ) {
+    selectCampo.empty().append($('<option>CARREGANDO</option>'));
     $.ajax('./ajax/getCampos.php', {
         data: {id: categoria},
         method: 'get',
         success: function ( response ) {
+            selectCampo.empty();
             const campos = JSON.parse(response);
             campos.reverse().forEach( function ( x ) {
                 const $opt = $('<option>'+x.name+'</option>');
@@ -41,9 +45,11 @@ function preencheCampo( categoria , selectCampo, selectValor ) {
 }
 
 function preencheResponsaveis( selectResponsaveis ) {
+    selectResponsaveis.empty().append($('<option>CARREGANDO</option>'));
     $.ajax('./ajax/getResponsaveis.php', {
         method: 'get',
         success: function ( response ) {
+            selectResponsaveis.empty();
             const responsaveis = JSON.parse(response);
             responsaveis.forEach( function ( x ) {
                 const $opt = $('<option>'+x.name+'</option>');
@@ -99,6 +105,7 @@ $(document).ready( function () {
         const $opt = $select.find(':selected');
         const type = $opt.attr('tp');
         const $valor = $select.parent().find('[name^=valor]');
+        const $operador = $select.parent().find('[name^=operador]');
         debugger;
         if (fixedValuesTypes.indexOf(type) == -1) {//campo livre
             if ($valor[0].tagName != 'INPUT') {
@@ -106,7 +113,12 @@ $(document).ready( function () {
                 $valor.replaceWith($input);
             }
         } else {
-            //TODO:
+            $operador.val('=');
+            if ($valor[0].tagName == 'INPUT') {
+                const $selectValor = $('<select name="valor[]">');
+                preencheValores($select.val(),$selectValor);
+                $valor.replaceWith($selectValor);
+            }
         }
     });
 
